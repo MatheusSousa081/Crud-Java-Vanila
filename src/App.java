6b8b7852c1ab564530f1ac71a0a437f2eae0ce28
MathesSousa081 <mc2019750@gmail.com>
2023-12-19 13:41:22 -0300

    Criado CRUD em java

--- src/App.java
+++ src/App.java
@@ -1,5 +1,39 @@
+import java.util.Scanner;
+
+import domain.SystemUsers;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        SystemUsers system = new SystemUsers();
+        Scanner sc = new Scanner(System.in);
+        int escolha = 0;
+
+        while (escolha != 5)  {
+            System.out.println("""
+                    ------ MENU ------
+                    1- Adicionar
+                    2- Listar
+                    3- Atualizar
+                    4- Remover
+                    5- Sair
+                    """);
+            
+            System.out.print("Escolha um opção: ");
+            escolha = sc.nextInt();
+            sc.nextLine();
+            System.out.println();
+
+            switch (escolha) {
+                case 1 -> system.createUser();
+                case 2 -> system.readUsers();
+                case 3 -> system.updateUser();
+                case 4 -> system.deleteUser();
+                case 5 -> System.out.println("Saindo...");
+                default -> System.out.println("Opção inválida! Tente novamente");
+            }
+        }
+
+        System.out.println("Programa encerrado!");
+        sc.close();
     }
 }
---
+++ src/domain/SystemUsers.java
@@ -0,0 +1,93 @@
+package domain;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Scanner;
+
+public class SystemUsers {
+    Scanner sc = new Scanner(System.in);
+    List<User> listUsers;
+
+    public SystemUsers() {
+        this.listUsers = new ArrayList<>();
+    }
+
+    public void createUser() {
+        System.out.print("Insira o nome do usuário: ");
+        String name = sc.nextLine();
+
+        System.out.print("Insira o email do usuário: ");
+        String email = sc.nextLine();
+
+        User user = new User(name, email);
+        listUsers.add(user);
+        System.out.println("Usuário inserido com sucesso!");
+        System.out.println();
+    }
+
+    public void readUsers() {
+        System.out.println("******* Lista *******");
+        System.out.println();
+        for (int i = 0; i < listUsers.size(); i++) {
+            System.out.println((i+1) + "- " + listUsers.get(i));
+            
+        }
+
+        System.out.println();
+        System.out.println("********************");
+        System.out.println();
+    }
+
+    public void updateUser() {
+        try {
+        readUsers();
+
+        System.out.print("Insira o indice do usuário que voce deseja atualizar: ");
+        int indice = sc.nextInt();
+        sc.nextLine();
+
+        if (indice > 0 && indice <= listUsers.size()) {
+            System.out.print("Insira o novo nome do usuário: ");
+            String newName = sc.nextLine();
+
+            System.out.print("Insira o novo email do usuário: ");
+            String newEmail = sc.nextLine();
+
+            listUsers.get((indice -1)).setName(newName);
+            listUsers.get((indice -1)).setEmail(newEmail);
+
+            System.out.println("Usuário atualizado com sucesso!");
+            System.out.println();
+
+        } else {
+            System.out.println("Indice invalido!");
+        }
+        System.out.println();
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida!");
+            sc.nextLine();
+        }
+    }   
+
+    public void deleteUser() {
+        try {
+            readUsers();
+
+            System.out.print("Insira o indice do usuário que voce deseja remover: ");
+            int indice = sc.nextInt();
+            sc.nextLine();
+
+            if (indice > 0 && indice >= listUsers.size()) {
+                listUsers.remove((indice - 1));
+                System.out.println("Usuário removido com sucesso!");
+            } else {
+                System.out.println("Indice invalido!");
+            }
+            System.out.println();
+        } catch (InputMismatchException e) {
+            System.out.println("Entrada inválida!");
+            sc.nextLine();
+        }
+    }
+}
